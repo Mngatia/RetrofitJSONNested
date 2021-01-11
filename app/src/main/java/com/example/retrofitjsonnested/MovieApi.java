f48b30d22ea5613a6280832976e8030f4b110241
Mngatia <nyawirahngatia@gmail.com>
2021-01-11 23:57:25 +0300

    Parsing JSON rom nested object using Retrofit.

--- app/src/main/java/com/example/retrofitjsonnested/MovieApi.java
+++ app/src/main/java/com/example/retrofitjsonnested/MovieApi.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 
 public interface MovieApi {
 //https://run.mocky.io/     v3/6360ab61-dfcf-4af1-b7b2-ee52d9081d7a
-    @GET("v3/b0b08dd4-9086-4e05-bb01-a2344545369f")
+    @GET("v3/6360ab61-dfcf-4af1-b7b2-ee52d9081d7a")
     Call<JSONResponse> getMovies();
 }
